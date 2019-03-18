c274aaa4fcc3fa1845f71e6217dfb25556607582
serkan <shaytac@gmail.com>
2019-03-18 17:15:15 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './app/index.js',
+  entry: "./app/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/"
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
+      template: "app/index.html"
     })
   ],
   devServer: {
-    historyApiFallback: true,
-  },
+    historyApiFallback: true
+  }
 };
