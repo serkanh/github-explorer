84167c88a1c4806c0c06377cb578fcfcee54c63f
serkan <shaytac@gmail.com>
2019-03-18 15:19:47 -0400

    fix

--- app/index.js
+++ app/index.js
@@ -1,8 +1,5 @@
-var React =  require('react');
-var ReactDom = require('react-dom');
-var App = require('./components/App')
-require('./index.css')
-ReactDom.render(
-	<App />,
-	document.getElementById('app')
-)
+var React = require("react");
+var ReactDom = require("react-dom");
+var App = require("./components/App");
+require("./index.css");
+ReactDom.render(<App />, document.getElementById("app"));
